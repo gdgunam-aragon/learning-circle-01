5fbd829135f4035f2684a5ddc2fab4d3d8451c34
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2019-11-02 13:01:50 -0600

    Added JSON Response on Flask

--- server.py
+++ server.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 
 
 @app.route('/hola-mundo')
-def hola():
+def hola_mundo():
     return 'Hola mundo'
 
-# :3
+
+@app.route('/hola-json')
+def hola_json():
+    response = {
+        'message': 'Hola mundo'
+    }
+    return json.dumps(response)
